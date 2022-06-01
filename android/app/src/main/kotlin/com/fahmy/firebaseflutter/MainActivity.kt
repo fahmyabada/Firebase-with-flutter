d886400af7da9e4f1da17e4155f319446525781c
FahmyAbada <fahmyabada@gmail.com>
2022-06-01 13:26:11 +0200

    1-6-2020

---
+++ android/app/src/main/kotlin/com/fahmy/firebaseflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fahmy.firebaseflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/notificationService/local_notification_service.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static void initialize() {
+    // initializationSettings  for Android
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+    );
+
+    _notificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: (String? id) async {
+        if (kDebugMode) {
+          print("onSelectNotification*******");
+        }
+        if (id!.isNotEmpty) {
+          if (kDebugMode) {
+            print("Custom data key ********* = $id");
+          }
+
+          // Navigator.of(context).push(
+          //   MaterialPageRoute(
+          //     builder: (context) => DemoScreen(
+          //       id: id,
+          //     ),
+          //   ),
+          // );
+
+        }
+      },
+    );
+  }
+
+  static void createAndDisplayNotification(RemoteMessage message) async {
+    try {
+      final id = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+      const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+          "fahmyabadanotificationapp",
+          "fahmyabadanotificationappchannel",
+          importance: Importance.max,
+          priority: Priority.high,
+        ),
+      );
+
+      await _notificationsPlugin.show(
+        id,
+        message.notification!.title,
+        message.notification!.body,
+        notificationDetails,
+        payload: message.data['id'],
+      );
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+}
